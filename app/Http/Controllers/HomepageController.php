2404645be0a3b69b6ad86c270116a6d426496b13
Miha Eržen <miha.erzen1@gmail.com>
2015-05-04 20:37:07 +0200

    Lumen init

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,13 @@
+<?php namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class HomepageController extends BaseController
+{
+    //
+    public function show()
+    {
+    	# code...
+    	return "Hello from HomepageController!";
+    }
+}
